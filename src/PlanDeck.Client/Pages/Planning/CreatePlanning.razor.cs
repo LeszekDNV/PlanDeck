30beace13aa5965ab10db7304811a44f78ae2de9
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2025-02-13 12:47:45 +0100

    Home view Create room view

---
+++ src/PlanDeck.Client/Pages/Planning/CreatePlanning.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using PlanDeck.Contracts.Room.Create;
+
+namespace PlanDeck.Client.Pages.Planning;
+
+public partial class CreatePlanning
+{
+    bool success;
+    private CreateRoomRequest model = new CreateRoomRequest();
+
+    private void OnValidSubmit(EditContext context)
+    {
+        success = true;
+        StateHasChanged();
+    }
+}
